5f20cfb4f3ee2bd24170bc46efb220ea1816dd08
Xiao-Ou Zhang <kepbod@gmail.com>
2016-05-29 16:54:56 +0800

    modify kepbod.gnome-terminal.sh

--- kepbod.gnome-terminal.sh
+++ kepbod.gnome-terminal.sh
@@ -2,45 +2,117 @@
 # kepbod - Gnome Terminal color scheme install script
 # Xiao-Ou Zhang <kepbod@gmail.com>
 
-[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="kepbod"
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Kepbod Color"
 [[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="kepbod"
+[[ -z "$DCONF" ]] && DCONF=dconf
+[[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen
+
+dset() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    if [[ "$type" == "string" ]]; then
+        val="'$val'"
+    fi
+
+    "$DCONF" write "$PROFILE_KEY/$key" "$val"
+}
+
+# because dconf still doesn't have "append"
+dlist_append() {
+    local key="$1"; shift
+    local val="$1"; shift
+
+    local entries="$(
+    {
+        "$DCONF" read "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+        echo "'$val'"
+    } | head -c-1 | tr "\n" ,
+    )"
+
+    "$DCONF" write "$key" "[$entries]"
+}
+
+# Newest versions of gnome-terminal use dconf
+if which "$DCONF" > /dev/null 2>&1; then
+    [[ -z "$BASE_KEY_NEW" ]] && BASE_KEY_NEW=/org/gnome/terminal/legacy/profiles:
+
+    if [[ -n "`$DCONF list $BASE_KEY_NEW/`" ]]; then
+        if which "$UUIDGEN" > /dev/null 2>&1; then
+            PROFILE_SLUG=`uuidgen`
+        fi
+
+        if [[ -n "`$DCONF read $BASE_KEY_NEW/default`" ]]; then
+            DEFAULT_SLUG=`$DCONF read $BASE_KEY_NEW/default | tr -d \'`
+        else
+            DEFAULT_SLUG=`$DCONF list $BASE_KEY_NEW/ | grep '^:' | head -n1 | tr -d :/`
+        fi
+
+        DEFAULT_KEY="$BASE_KEY_NEW/:$DEFAULT_SLUG"
+        PROFILE_KEY="$BASE_KEY_NEW/:$PROFILE_SLUG"
+
+        # copy existing settings from default profile
+        $DCONF dump "$DEFAULT_KEY/" | $DCONF load "$PROFILE_KEY/"
+
+        # add new copy to list of profiles
+        dlist_append $BASE_KEY_NEW/list "$PROFILE_SLUG"
+
+        # update profile values with theme options
+        dset visible-name "'$PROFILE_NAME'"
+        dset palette "['#272822', '#fb9fb1', '#a6e22e', '#fd971f', '#66d9ef', '#ae81ff', '#a1efe4', '#f5f4f1', '#383830', '#deaf8f', '#acc267', '#f4bf75', '#6fc2ef', '#e1a3ee', '#12cfc0', '#f9f8f5']"
+        dset background-color "'#272822'"
+        dset foreground-color "'#f9f8f5'"
+        dset bold-color "'#f9f8f5'"
+        dset bold-color-same-as-fg "true"
+        dset use-theme-colors "false"
+        dset use-theme-background "false"
+
+        unset PROFILE_NAME
+        unset PROFILE_SLUG
+        unset DCONF
+        unset UUIDGEN
+        exit 0
+    fi
+fi
+
+# Fallback for Gnome 2 and early Gnome 3
 [[ -z "$GCONFTOOL" ]] && GCONFTOOL=gconftool
 [[ -z "$BASE_KEY" ]] && BASE_KEY=/apps/gnome-terminal/profiles
 
 PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
 
 gset() {
-  local type="$1"; shift
-  local key="$1"; shift
-  local val="$1"; shift
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
 
-  "$GCONFTOOL" --set --type "$type" "$PROFILE_KEY/$key" -- "$val"
+    "$GCONFTOOL" --set --type "$type" "$PROFILE_KEY/$key" -- "$val"
 }
 
 # Because gconftool doesn't have "append"
 glist_append() {
-  local type="$1"; shift
-  local key="$1"; shift
-  local val="$1"; shift
+    local type="$1"; shift
+    local key="$1"; shift
+    local val="$1"; shift
 
-  local entries="$(
+    local entries="$(
     {
-      "$GCONFTOOL" --get "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
-      echo "$val"
+        "$GCONFTOOL" --get "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+        echo "$val"
     } | head -c-1 | tr "\n" ,
-  )"
+    )"
 
-  "$GCONFTOOL" --set --type list --list-type $type "$key" "[$entries]"
+    "$GCONFTOOL" --set --type list --list-type $type "$key" "[$entries]"
 }
 
 # Append the kepbod profile to the profile list
 glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
 
 gset string visible_name "$PROFILE_NAME"
-gset string palette "#227722882222:#ffbb99ffbb11:#aa66ee2222ee:#ffdd997711ff:#6666dd99eeff:#aaee8811ffff:#aa11eeffee44:#ff55ff44ff11:#338833883300:#ddeeaaff88ff:#aacccc226677:#ff44bbff7755:#66ffcc22eeff:#ee11aa33eeee:#1122ccffcc00:#ff99ff88ff55"
-gset string background_color "#227722882222"
-gset string foreground_color "#ff99ff88ff55"
-gset string bold_color "#ff99ff88ff55"
+gset string palette "#272822:#fb9fb1:#a6e22e:#fd971f:#66d9ef:#ae81ff:#a1efe4:#f5f4f1:#383830:#deaf8f:#acc267:#f4bf75:#6fc2ef:#e1a3ee:#12cfc0:#f9f8f5"
+gset string background_color "#272822"
+gset string foreground_color "#f9f8f5"
+gset string bold_color "#f9f8f5"
 gset bool bold_color_same_as_fg "true"
 gset bool use_theme_colors "false"
 gset bool use_theme_background "false"
